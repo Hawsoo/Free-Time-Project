d51e6b20dc2e477162dea249747c33ab4eb7e402
Timothy Bennett <timoguy99004@gmail.com>
2015-06-11 16:53:32 -0700

    Starts ball physics

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour
+{
+	public float maxSpeed;
+	public float jumpHeight;
+
+	public float gravity;
+
+	// Update
+	void FixedUpdate()
+	{
+		Vector3 velocity = GetComponent<Rigidbody> ().velocity;
+
+		// Move
+		velocity.x = Input.GetAxis ("Horizontal") * maxSpeed;
+		velocity.z = Input.GetAxis("Vertical") * maxSpeed;
+
+		// Jump
+		if (Input.GetKey(KeyCode.Space))
+			velocity.y = jumpHeight;
+
+		// Apply gravity
+		velocity.y -= gravity;
+
+		// Apply movement
+		GetComponent<Rigidbody> ().velocity = velocity;
+	}
+}
